a29e42128f32c29310c76b0f5b68546c5987bb3d
Onur Atakan ULUSOY <atadogan06@gmail.com>
2022-09-11 00:12:35 +0300

    Fix try for arguments

--- src/speed_calculator/speed_calculator.py
+++ src/speed_calculator/speed_calculator.py
@@ -2,7 +2,7 @@ import time
 
 def calculate(function, *args, result=False):
     start_time = time.time()
-    result = function(args)
+    result = function(*args)
     end_time = time.time()
     the_return = end_time - start_time
     if result:
