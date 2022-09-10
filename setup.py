c4fea7e3e7e681fe3c65914e6835715cf20b7735
Onur Atakan ULUSOY <atadogan06@gmail.com>
2022-09-10 17:03:46 +0300

    Removed integer transformation

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(name='speed_calculator',
-version='0.1.1',
+version='0.2.0',
 description="""A library to speed calculation of a function.""",
 long_description="""
 # Speed Calculator
--- src/speed_calculator/speed_calculator.py
+++ src/speed_calculator/speed_calculator.py
@@ -4,4 +4,4 @@ def calculate(function):
     start_time = time.time()
     function()
     end_time = time.time()
-    return int(end_time - start_time)
+    return end_time - start_time
